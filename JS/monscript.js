cd4d3eec81d5198f3509e5fd21f5cc1c82630cc7
Damien <damien.deboudt@sfr.fr>
2023-01-09 16:44:19 +0100

    en cours, menu ok

--- JS/monscript.js
+++ JS/monscript.js
@@ -1,11 +1,11 @@
-let menu = document.getElementById("menu")
+let menu = document.getElementById("menu");
 
 function menuVisible (){
-    menu.style.transform = 'translateY(0px)'
-    menu.style.zIndex = '1'
+    menu.style.transform = 'translateY(0px)';
+    menu.style.zIndex = '1';
 }
 
-document.getElementById("buttonMenu").addEventListener("click", menuVisible)
+document.getElementById("buttonMenu").addEventListener("click", menuVisible);
  document.getElementById("closemenu").addEventListener("click", ()=>{
-     menu.style.zIndex = '-1'
+     menu.style.zIndex = '-1';
  })
